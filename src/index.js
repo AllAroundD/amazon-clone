a1f1090b52aea29f0ce21c5207eadcb04b7fe014
AllAroundD <AllAroundDWeb@gmail.com>
2020-11-22 11:15:03 -0500

    added meta tags and updated logo

--- src/index.js
+++ src/index.js
@@ -5,12 +5,24 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer, { initialState } from "./reducer";
 import { StateProvider } from "./StateProvider";
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+const options = {
+  position: positions.TOP_RIGHT,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
+    </AlertProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
